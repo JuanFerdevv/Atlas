fa7f42ea38f92d9caca18ae238c997004afe4587
Juan Calvache <juancalvache009@gmail.com>
2024-04-23 17:57:10 -0500

    Fin sección 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkWithHref, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { ProyectosComponent } from './pages/proyectos/proyectos.component';
-
+import { ContactComponent } from './pages/contact/contact.component';
+import { IndexComponent } from './pages/index/index.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent, FooterComponent, ProyectosComponent],
+  imports: [RouterOutlet,RouterLink,HeaderComponent, FooterComponent, ProyectosComponent,
+    ContactComponent,IndexComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ShopComponent } from './pages/shop/shop.component';
+import { ProyectosComponent } from './pages/proyectos/proyectos.component';
+import { IndexComponent } from './pages/index/index.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:IndexComponent},
+    {path:'proyectos',component:ProyectosComponent},
+    {path:'contact', component:ContactComponent},
+    {path:'shop',component:ShopComponent},
+];
