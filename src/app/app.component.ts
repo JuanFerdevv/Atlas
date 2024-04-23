96b94a09e12e0bab1a6b7c3a61dfb21dbf855119
Juan Calvache <juancalvache009@gmail.com>
2024-04-23 10:20:14 -0500

    Header footer y proyectos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ProyectosComponent } from './pages/proyectos/proyectos.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent, FooterComponent, ProyectosComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Atlas';
