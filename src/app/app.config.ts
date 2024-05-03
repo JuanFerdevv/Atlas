a71b8beda0b9c82afaf2f9983777d65bc249a349
Juan Calvache <juancalvache009@gmail.com>
2024-05-03 16:39:03 -0500

    4:39

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-<<<<<<< HEAD
 import { provideRouter, withHashLocation} from '@angular/router';
-=======
-import { provideRouter, withHashLocation } from '@angular/router';
->>>>>>> nueva-rama
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -11,9 +7,5 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideServerRendering } from '@angular/platform-server';
 
 export const appConfig: ApplicationConfig = {
-<<<<<<< HEAD
   providers: [provideRouter(routes, withHashLocation()), provideServerRendering(), provideClientHydration(),provideHttpClient(withFetch())]
-=======
-  providers: [provideRouter(routes, withHashLocation()), provideClientHydration(),provideHttpClient(withFetch())]
->>>>>>> nueva-rama
 };
